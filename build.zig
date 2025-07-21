99941bf109f5e94bd7ca2653001ac4706283f1d7
Q <264902+kofi-q@users.noreply.github.com>
2025-07-20 21:27:19 -0500

    Try forcing the LLVM backend to unbreak CI

--- build.zig
+++ build.zig
@@ -132,6 +132,7 @@ pub fn build(b: *std.Build) void {
             .target = target,
             .tokota = .{ .dep = &dep_tokota_internal },
         });
+        addon.lib.use_llvm = true;
 
         steps.check.dependOn(&b.addLibrary(.{
             .name = blk: {
